3f7a65e424571ce630500ac3b22dc3549b4d420f
4b <4b@labinf1-07>
2023-03-04 12:10:09 +0100

    Creazione del metodo calcolaArea 04/03/2023

--- src/Triangolo.java
+++ src/Triangolo.java
@@ -1,4 +1,40 @@
 
 public class Triangolo {
 
+	private float base, altezza;
+	
+	public Triangolo() {
+		
+		base=0;
+		altezza=0;
+	}
+	
+	public float AreaTriangoolo() {
+		
+		return base*altezza;
+	}
+
+	public float getBase() {
+		return base;
+	}
+
+	public void setBase(float base) {
+		this.base = base;
+	}
+
+	public float getAltezza() {
+		return altezza;
+	}
+
+	public void setAltezza(float altezza) {
+		this.altezza = altezza;
+	}
+
+	@Override
+	public String toString() {
+		return "Triangolo [base=" + base + ", altezza=" + altezza + "]";
+	}
+	
+	
+	
 }
